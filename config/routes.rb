01aeafe1fa77ceed473b6efd3349f844cd59ffd2
Nick Lauwers <nicholaslauwers04@gmail.com>
2019-08-16 22:06:01 +0000

    Add users via devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root           'static_pages#home'
   
   get 'about' => 'static_pages#about' 
+  
+  devise_for :users, 
+             controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 end
